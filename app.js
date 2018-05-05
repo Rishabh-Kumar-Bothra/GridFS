cd59e61a46b4b10ccc9f9985702a8f1b4d729963
RishabhKr bothra <rbothra77@gmail.com>
2018-05-06 00:54:11 +0530

    able to access back the image as json file

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ const mongoURI = "mongodb://grid:12345@ds115420.mlab.com:15420/mongouploads";
 
 const conn = mongoose.createConnection(mongoURI);
 
+let gfs;
 conn.once('open', ()=> {
-    var gfs = Grid(conn.db, mongoose.mongo);
+     gfs = Grid(conn.db, mongoose.mongo);
     gfs.collection('uploads');
 })
 
@@ -53,7 +54,51 @@ app.get('/',(req,res)=>{
 
 app.post('/upload',upload.single('file'),(req,res)=>{
 
-    res.json({file : req.file})
+   // res.json({file : req.file})
+    res.redirect('/')
+})
+
+app.get('/files',(req,res)=>{
+    gfs.files.find().toArray((err,files)=>{
+        if(!files || files.length === 0){
+            return res.status(404).json({err:'No file exist'})
+        }
+        res.json(files);
+    })
+
+
+})
+
+app.get('/files/:filename',(req,res)=>{
+    gfs.files.findOne({filename:req.params.filename},(err,file)=>{
+        if(!file || file.length === 0){
+            return res.status(404).json({err:'No file exist'});
+        }
+        res.json(file);
+
+    })
+
+
+})
+
+
+
+app.get('/image/:filename',(req,res)=>{
+    gfs.files.findOne({filename:req.params.filename},(err,file)=>{
+        if(!file || file.length === 0){
+            return res.status(404).json({err:'No file exist'});
+        }
+        if(file.contentType==='image/jpeg' || file.contentType==='image/png'){
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        }
+        else{
+            return res.status(404).json({err:'No file exist'});
+        }
+
+    })
+
+
 })
 
 
